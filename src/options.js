9428f4bda417357981324f52385bf9e44ccebf42
Nick <methodgr4b@gmail.com>
2020-05-10 17:35:51 +0100

    :sparkles: Add custom background colour option (fixes #6)

--- src/options.js
+++ src/options.js
@@ -1,3 +1,15 @@
+const hiddenClass = 'is-hidden';
+
+const updateCustomBackgroundColorVisibility = _ => {
+	const theme = document.getElementById( 'theme' ).value;
+
+	if ( theme === 'custom' ) {
+		document.getElementById( 'customBackgroundColor' ).classList.remove( hiddenClass );
+	} else {
+		document.getElementById( 'customBackgroundColor' ).classList.add( hiddenClass );
+	}
+};
+
 const saveOptions = e => {
 	e.preventDefault();
 
@@ -5,17 +17,22 @@ const saveOptions = e => {
 		customNewTabUrl: document.getElementById( 'customNewTabUrl' ).value,
 		customNewTabTitle: document.getElementById( 'customNewTabTitle' ).value,
 		theme: document.getElementById( 'theme' ).value,
+		customBackgroundColor: document.getElementById( 'customBackgroundColor' ).value,
 	});
 };
 
 const restoreOptions = _ => {
-	browser.storage.sync.get([ 'customNewTabUrl', 'customNewTabTitle', 'theme' ])
+	browser.storage.sync.get([ 'customNewTabUrl', 'customNewTabTitle', 'theme', 'customBackgroundColor' ])
 		.then( options => {
 			document.getElementById( 'customNewTabUrl' ).value = options.customNewTabUrl || '';
 			document.getElementById( 'customNewTabTitle' ).value = options.customNewTabTitle || '';
 			document.getElementById( 'theme' ).value = options.theme || 'none';
+			document.getElementById( 'customBackgroundColor' ).value = options.customBackgroundColor || '';
+
+			updateCustomBackgroundColorVisibility();
 		});
 };
 
 document.addEventListener( 'DOMContentLoaded', restoreOptions );
 document.querySelector( 'form' ).addEventListener( 'submit', saveOptions );
+document.getElementById( 'theme' ).addEventListener( 'change', updateCustomBackgroundColorVisibility );
